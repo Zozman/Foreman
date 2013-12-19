ea7e718a5ac57a25ad0b39a3aa88f051ca95d3ca
Zac Lovoy <zwlovoy@gmail.com>
2013-12-18 22:11:39 -0600

    Icons
    Added Icon

--- Foreman/StatsViewController.m
+++ Foreman/StatsViewController.m
@@ -43,7 +43,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+        
     // Initialize background task queue
     backgroundQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
     
